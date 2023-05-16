7f042f011996c26a07071de4bacd9b1c71e86277
sorab <sorabkumarsk.sk@gmail.com>
2023-05-16 10:41:49 +0500

    Added csv

--- app4.py
+++ app4.py
@@ -6,28 +6,31 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
 
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import TextLoader,CSVLoader
 from langchain.chains import RetrievalQA,ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 
 
 def get_response(prompt, conversation):
-    print("none type",conversation)
     os.environ["OPENAI_API_KEY"] = apikey
 
     loader = TextLoader("./train.txt")
     documents = loader.load()
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    documents = text_splitter.split_documents(documents)
+    product_loader = CSVLoader("./products.csv")
+    product_documents = product_loader.load()
 
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    documentsCollection = documents + product_documents
+
+    text_splitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=0)
+    documents = text_splitter.split_documents(documentsCollection)
+
 
     embeddings = OpenAIEmbeddings()
     db = Chroma.from_documents(documents, embeddings)
     retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
 
-    llm = OpenAI(temperature=0.9)
+    llm = OpenAI(temperature=0.4)
 
     chat_history = []
 
@@ -38,14 +41,12 @@ def get_response(prompt, conversation):
                 answer = conversation[i]["content"]
                 chat_history.append((query, answer))
 
-    print(chat_history)
 
     # Create a RetrievalQA
     qa = ConversationalRetrievalChain.from_llm(
         llm=llm, retriever=retriever, return_source_documents=False)
 
     result = qa({"question": prompt,'chat_history': chat_history})
-    print('result', result)
     return result
 
 
